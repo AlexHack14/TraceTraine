a54e9126e439896f6d04c7e606f6edecf5041f6f
Oleksiy Radyuk <mi.alexhack14@gmail.com>
2020-03-01 00:45:50 +0200

    init project

---
+++ src/main/java/com/edu/lnu/pma/trace_traine/TraceTraineApplication.java
@@ -0,0 +1,13 @@
+package com.edu.lnu.pma.trace_traine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceTraineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TraceTraineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/edu/lnu/pma/trace_traine/TraceTraineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.lnu.pma.trace_traine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceTraineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
